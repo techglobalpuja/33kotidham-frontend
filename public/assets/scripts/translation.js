1b1a94887e0f8a9bc3e1e42cce8dd7e344b97eac
Aman Thakur <amansinghthakur252002@gmail.com>
2025-09-24 21:51:32 +0530

    langauage change top bar removed

--- public/assets/scripts/translation.js
+++ public/assets/scripts/translation.js
@@ -4,6 +4,11 @@ function googleTranslateElementInit() {
   }
   new google.translate.TranslateElement({
     pageLanguage: window.__GOOGLE_TRANSLATION_CONFIG__.defaultLanguage,
+    autoDisplay: false,
+    includedLanguages: 'en,hi,gu',
+    layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+    gaTrack: false,
+    gaId: '',
   }, 'google_translate_element');
 
   
