1d79885f423681d45e6c1b27bb6c4c99a2c960e1
Vicky <vivekanandnayak05@gmail.com>
2025-12-05 02:03:47 +0530

    hero section

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -7,20 +7,20 @@ const HeroSection: React.FC = () => {
   const heroSlides = [
     {
       id: 1,
-      desktopImage: '/images/home/hero1/desktop/2024-11-05T08_05_37.142Z-Panchmukhi Hanuman_Website.png',
-      mobileImage: '/images/home/hero1/mobile/2024-11-05T08_05_34.596Z-Panchmukhi Hanuman_Responsive.png',
-      alt: 'Panchmukhi Hanuman Puja'
+      desktopImage: '/images/home/hero1/desktop/hero_slide_1.webp',
+      mobileImage: '/images/home/hero1/mobile/hero_slide_1_mobile.webp',
+      alt: 'Maa Baglamukhi Puja'
     },
     {
       id: 2,
-      desktopImage: '/images/home/hero2/desktop/2024-10-24T13_46_40.726Z-Sarva Karya Siddhi Puja web.png',
-      mobileImage: '/images/home/hero2/mobile/2024-10-24T13_46_34.486Z-Baglamukhi Responsive.png',
-      alt: 'Sarva Karya Siddhi Puja'
+      desktopImage: '/images/home/hero2/desktop/hero_slide_2.webp',
+      mobileImage: '/images/home/hero2/mobile/hero_slide_2_mobile.webp',
+      alt: 'Kundali Dosh Nivaran Shani Puja'
     },
     {
       id: 3,
-      desktopImage: '/images/home/hero3/desktop/2024-10-24T13_17_20.561Z-Rahu-Ketu Shanti Puja_Puja web 1.png',
-      mobileImage: '/images/home/hero3/mobile/2024-10-24T13_17_14.434Z-rahu ketu mobile response.png',
+      desktopImage: '/images/home/hero3/desktop/hero_slide_3 (2).webp',
+      mobileImage: '/images/home/hero3/mobile/hero_slide_3_mobile.webp',
       alt: 'Rahu-Ketu Shanti Puja'
     }
   ];
